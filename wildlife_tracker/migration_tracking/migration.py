6200e811af4e264c10e092d8a00562aaf6349908
Varuni Gupta <85125668+varuni24@users.noreply.github.com>
2024-10-03 14:34:46 -0400

    Add files via upload

---
+++ wildlife_tracker/migration_tracking/migration.py
@@ -0,0 +1,12 @@
+from typing import Any
+from migration_path import MigrationPath
+
+
+class Migration:
+    def __init__(self, migration_id: int, migration_path: MigrationPath, current_location: str, current_date: str, start_date: str, status: str = "Scheduled"):
+        self.migration_id = migration_id
+        self.migration_path = migration_path
+        self.status = status
+        self.current_location = current_location
+        self.start_date = start_date
+        self.current_date = current_date
